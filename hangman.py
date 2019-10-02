e65659b6433d95fa64e405bb083fca733b46f65a
Isaiah Carter <izay.carter0509@gmail.com>
2019-10-01 20:07:29 -0400

    hangman games

---
+++ hangman.py
@@ -0,0 +1,155 @@
+import random
+
+randomlst = [
+    'people',
+    'history',
+    'way',
+    'art',
+    'world',
+    'information',
+    'map',
+    'two',
+    'family',
+    'government',
+    'health',
+    'system',
+    'computer',
+    'meat',
+    'year',
+    'thanks',
+    'music',
+    'person',
+    'reading',
+    'method',
+    'data',
+    'food',
+    'understanding',
+    'theory',
+    'law',
+    'bird',
+    'literature',
+    'problem',
+    'software',
+    'control',
+    'knowledge',
+    'power',
+    'ability',
+    'economics',
+    'love',
+    'internet',
+    'television',
+    'science',
+    'library',
+    'nature',
+    'fact',
+    'product',
+    'idea',
+    'temperature',
+    'investment',
+    'area',
+    'society',
+    'activity',
+    'story',
+    'industry',
+    'media',
+    'thing',
+    'oven',
+    'community',
+    'definition',
+    'safety',
+    'quality',
+    'development',
+    'language',
+    'management',
+    'player',
+    'variety',
+    'video',
+    'week',
+    'security',
+    'country',
+    'exam',
+    'movie',
+    'organization',
+    'equipment',
+    'physics',
+    'analysis',
+    'policy',
+    'series',
+    'thought',
+    'basis',
+    'boyfriend',
+    'direction',
+    'strategy',
+    'technology',
+    'army',
+    'camera',
+    'freedom',
+    'paper',
+    'environment',
+    'child',
+    'instance',
+    'month',
+    'truth',
+    'marketing',
+    'university',
+    'writing',
+    'article',
+    'department',
+    'difference',
+    'goal',
+    'news',
+    'audience',
+    'fishing',
+    'growth',
+    'income',
+    'marriage',
+    'user',
+    'combination']
+
+randomword = (" ".join([randomlst[random.randrange(0, len(randomlst))]]))
+
+word = randomword
+
+print("Welcome to Hangman!! \n")
+
+print(word)
+
+#runs untill errors is 6 for the mistakes
+guessed = []
+wrong = []
+
+tries = 7
+
+while tries > 0:
+
+    out = ""
+    for letter in word:
+        if letter in guessed:
+            out = out + letter
+        else:
+            out = out + "_"
+
+    if out == word:
+        break
+
+    print("Guess the word:", out)
+    print(tries, "chances left")
+
+    guess = input()
+
+    if guess in guessed or guess in wrong:
+        print("Already guessed", guess)
+    elif guess in word:
+        print("Yay")
+        guessed.append(guess)
+    else:
+        print("Nope")
+        tries = tries - 1
+        wrong.append(guess)
+
+    print()
+
+if tries:
+    print("You guessed", word)
+else:
+    print("You didn't guess", word)
